e02800cc93a61b1033fd4f6f163deaf479267973
Wyatt Bristow <wyattbristow@hotmail.com>
2026-03-12 08:42:35 -0600

    Retain ui state between basic and advanced results view

--- H.GUI.Avalonia/H.Avalonia/ViewModels/Results/ResultsSidebarViewModel.cs
+++ H.GUI.Avalonia/H.Avalonia/ViewModels/Results/ResultsSidebarViewModel.cs
@@ -6,6 +6,7 @@ using H.Avalonia.Views.ResultViews;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,15 @@ namespace H.Avalonia.ViewModels.Results
     {
         #region Fields
 
-        private object _selectedItem;
         private bool _isAdvancedMode = false;
-        private object _lastSelectedAdvancedItem;
+
+        private ListBoxItem _selectedBasicItem;
+        private ListBoxItem _selectedAdvancedItem;
+        private ListBoxItem _lastSelectedBasicItem;
+        private ListBoxItem _lastSelectedAdvancedItem;
+
+        private ObservableCollection<ListBoxItem> _basicViewChapters = new();
+        private ObservableCollection<ListBoxItem> _advancedViewTabs = new();
 
         #endregion
 
@@ -26,31 +33,71 @@ namespace H.Avalonia.ViewModels.Results
 
         public ResultsSidebarViewModel(IRegionManager regionManager) : base(regionManager)
         {
-
+            PopulateBasicChaptersTitles();
+            PopulateAdvancedTabs();
+            this.PropertyChanged += OnSelectedOptionChanged;
         }
 
         #endregion
 
         #region Properties
 
-        public object SelectedItem
+        /// <summary>
+        /// The currently selected chapter in the basic view. Changing this will navigate to the corresponding chapter in the summary view.
+        /// </summary>
+        public ListBoxItem SelectedBasicItem
         {
-            get => _selectedItem;
-            set => SetProperty(ref _selectedItem, value);
-        }
-
-        public object LastSelectedAdvancedItem
-        {
-            get => _lastSelectedAdvancedItem;
+            get => _selectedBasicItem;
             set
             {
-                if (!IsAdvancedMode)
-                {
-                    SetProperty(ref _lastSelectedAdvancedItem, value);
-                }
+                SetProperty(ref _selectedBasicItem, value);
             }
         }
 
+        /// <summary>
+        /// The currently selected tab in the advanced view. Changing this will navigate to the corresponding tab in the advanced results view.
+        /// </summary>
+        public ListBoxItem SelectedAdvancedItem
+        {
+            get => _selectedAdvancedItem;
+            set
+            {
+                SetProperty(ref _selectedAdvancedItem, value);
+            }
+
+        }
+
+        /// <summary>
+        /// An observable collection of chapters to display in the sidebar when in basic mode. The first item will be selected by default if no chapter is currently selected.
+        /// </summary>
+        public ObservableCollection<ListBoxItem> BasicViewChapters
+        {
+            get
+            {
+                if (SelectedBasicItem == null)
+                    SelectedBasicItem = _basicViewChapters.FirstOrDefault();
+                return _basicViewChapters;
+            }
+            set => SetProperty(ref _basicViewChapters, value);
+        }
+
+        /// <summary>
+        /// An observable collection of tabs to display in the sidebar when in advanced mode. The first item will be selected by default if no tab is currently selected.
+        /// </summary>
+        public ObservableCollection<ListBoxItem> AdvancedViewTabs
+        {
+            get
+            {
+                if (SelectedAdvancedItem == null)
+                    SelectedAdvancedItem = _advancedViewTabs.FirstOrDefault();
+                return _advancedViewTabs;
+            }
+            set => SetProperty(ref _advancedViewTabs, value);
+        }
+
+        /// <summary>
+        /// Boolean that indicates whether the sidebar is in basic or advanced mode. 
+        /// </summary>
         public bool IsAdvancedMode
         {
             get => _isAdvancedMode;
@@ -85,37 +132,74 @@ namespace H.Avalonia.ViewModels.Results
 
         private void OnSelectedOptionChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if (SelectedItem is ListBoxItem item && item.Content != null)
+            if (SelectedAdvancedItem is ListBoxItem item && item.Content != null)
             {
                 string? selectedOption = item.Content.ToString();
-                switch (selectedOption)
+                if (!IsAdvancedMode)
                 {
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleMultiYearCarbonModelling:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(MultiYearCarbonModellingView));
-
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleEstimatesOfProduction:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(EstimatesOfProductionView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleFeedEstimateReport:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(FeedEstimateReportView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.LabelManureManagement:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ManureManagementResultsView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleEmissionsPieChart:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(EmissionPieChartView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleOverallEmissions:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(OverallEmissionsResultsView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleComponentEmissions:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ComponentEmissionsResultsView));
-                        break;
-                    case var _ when selectedOption == H.Core.Properties.Resources.TitleDetailedEmissionsReport:
-                        base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(DetailedEmissionsReportResultsView));
-                        break;
+                    NavigateToBasicChapter(selectedOption);
                 }
+                else
+                    NavigateToAdvancedTab(selectedOption);
+            }
+        }
+
+        private void PopulateBasicChaptersTitles()
+        {
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Farm Profile" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Annual Production Summary" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Total GHG Emissions" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Emissions Breakdown By Category" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Carbon Sequestration Summary" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Manure Management Overview" });
+            _basicViewChapters.Add(new ListBoxItem() { Content = "Key Findings and Recommendations" });
+        }
+
+        private void PopulateAdvancedTabs()
+        {
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleMultiYearCarbonModelling });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleEstimatesOfProduction });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleFeedEstimateReport });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.LabelManureManagement });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleEmissionsPieChart });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleOverallEmissions });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleComponentEmissions });
+            _advancedViewTabs.Add(new ListBoxItem() { Content = H.Core.Properties.Resources.TitleDetailedEmissionsReport });
+        }
+
+        private void NavigateToBasicChapter(string selectedOption)
+        {
+            selectedOption = "ResultsSummaryView";
+            base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ResultsSummaryView));
+        }
+        private void NavigateToAdvancedTab(string selectedOption)
+        {
+            switch (selectedOption)
+            {
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleMultiYearCarbonModelling:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(MultiYearCarbonModellingView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleEstimatesOfProduction:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(EstimatesOfProductionView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleFeedEstimateReport:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(FeedEstimateReportView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.LabelManureManagement:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ManureManagementResultsView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleEmissionsPieChart:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(EmissionPieChartView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleOverallEmissions:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(OverallEmissionsResultsView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleComponentEmissions:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ComponentEmissionsResultsView));
+                    break;
+                case var _ when selectedOption == H.Core.Properties.Resources.TitleDetailedEmissionsReport:
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(DetailedEmissionsReportResultsView));
+                    break;
             }
         }
 
@@ -123,25 +207,29 @@ namespace H.Avalonia.ViewModels.Results
 
         #region Event Handlers
 
-        public override void OnNavigatedTo(NavigationContext navigationContext)
-        {
-            this.PropertyChanged += OnSelectedOptionChanged;
-            SelectedItem = new ListBoxItem { Content = H.Core.Properties.Resources.TitleMultiYearCarbonModelling };
-        }
-
         private void OnResultModeChanged()
         {
+            _lastSelectedBasicItem = _selectedBasicItem;
+            _lastSelectedAdvancedItem = _selectedAdvancedItem;
             // Store current advanced tab, navigate to summary when switching to basic
             if (!IsAdvancedMode)
             {
-                //_lastSelectedAdvancedItem = _selectedItem;
-                base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ResultsSummaryView));
+                if (_lastSelectedBasicItem == null)
+                {
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(ResultsSummaryView));
+                    return;
+                }
+                SelectedBasicItem = _lastSelectedBasicItem;
             }
-            // Restore last advanced tab if switching back to advanced
             else
             {
-                //SelectedItem = _lastSelectedAdvancedItem;
-                base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(MultiYearCarbonModellingView));
+                if (_lastSelectedAdvancedItem == null)
+                {
+                    base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(MultiYearCarbonModellingView));
+                    return;
+                }
+
+                base.RegionManager.RequestNavigate(UiRegions.ContentRegion, nameof(_lastSelectedAdvancedItem));
             }
         }
 
--- H.GUI.Avalonia/H.Avalonia/Views/ResultViews/ResultsSummaryView.axaml.cs
+++ H.GUI.Avalonia/H.Avalonia/Views/ResultViews/ResultsSummaryView.axaml.cs
@@ -1,13 +1,60 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Prism.Regions;
 
 namespace H.Avalonia.Views.ResultViews;
 
-public partial class ResultsSummaryView : UserControl
+public partial class ResultsSummaryView : UserControl, INavigationAware
 {
     public ResultsSummaryView()
     {
         InitializeComponent();
     }
+
+    public void OnNavigatedTo(NavigationContext navigationContext)
+    {
+        if (navigationContext?.Parameters != null && navigationContext.Parameters.ContainsKey("chapter"))
+        {
+            var chapter = navigationContext.Parameters.GetValue<string>("chapter");
+            if (!string.IsNullOrEmpty(chapter))
+            {
+                // Post to UI thread so layout is complete before BringIntoView
+                Dispatcher.UIThread.Post(() => ScrollToChapter(chapter), DispatcherPriority.Background);
+            }
+        }
+    }
+
+    private void ScrollToChapter(string chapter)
+    {
+        // Named Borders in the XAML become fields: FarmProfileSection, AnnualProductionSection, etc.
+        switch (chapter)
+        {
+            case "FarmProfileSection":
+                FarmProfileSection?.BringIntoView();
+                break;
+            case "AnnualProductionSection":
+                AnnualProductionSection?.BringIntoView();
+                break;
+            case "TotalGHGEmissionsSection":
+                TotalGHGEmissionsSection?.BringIntoView();
+                break;
+            case "EmissionsBreakdownSection":
+                EmissionsBreakdownSection?.BringIntoView();
+                break;
+            case "CarbonSequestrationSection":
+                CarbonSequestrationSection?.BringIntoView();
+                break;
+            case "ManureManagementSection":
+                ManureManagementSection?.BringIntoView();
+                break;
+            case "KeyFindingsSection":
+                KeyFindingsSection?.BringIntoView();
+                break;
+        }
+    }
+
+    public bool IsNavigationTarget(NavigationContext navigationContext) => true;
+    public void OnNavigatedFrom(NavigationContext navigationContext) { }
 }
